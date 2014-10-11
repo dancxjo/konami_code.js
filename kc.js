0d6b7b120878d72a04712a380905ee82565f62c8
dancxjo <tdreed@gmail.com>
2014-10-11 15:39:42 -0700

    Update kc.js

--- kc.js
+++ kc.js
@@ -1,4 +1,32 @@
-function KonamiCodeDetector {
-  var key = {U:38, D:40, L:37, R:39, A:, B:};
-  this.code_entered
+function KonamiCodeDetector(handler, keys, code) {
+  this.keys = keys;
+  this.code = code;
+  this.soFar = 0;
+  this.handler = handler;
+  window.addEventListener('keypress', this.listener, false);
 }
+
+KonamiCodeDetector.prototype = {
+  code: [38,38,40,40,37,39,37,39,17,18],
+  handler: function (event) { 
+    console.log('30 lives'); 
+  },
+  listener: function (e) {
+    if (this.code[this.soFar] == e.keyCode) {
+      this.soFar++;
+    } else {
+      this.reset();
+    }
+    
+    if (this.soFar == this.code.length) {
+      this.handler();
+      this.reset();
+    }
+  },
+  reset: function () {
+    this.soFar = [];
+  }.
+  detach: function () {
+    window.removeEventListener('keypress', this.listener);
+  }
+};
